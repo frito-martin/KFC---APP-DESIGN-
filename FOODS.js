e4a3f7745e5d7b4c599c681c6273f45033ef09fb
Muzei frito <139154552+frito-martin@users.noreply.github.com>
2023-12-01 08:12:13 -0500

    Add files via upload

---
+++ FOODS.js
@@ -0,0 +1,149 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Chocolate',
+    category : 'icecream',
+    rating : 4.3,
+    price: 10,
+    img: 'images/icecream/Chocolate-Sundae-e1563467418861.webp',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'goldenIce',
+    category : 'icecream',
+    rating : 4.3,
+    price: 40,
+    img: 'images/icecream/kentucky-fried-chickenflavored-ice-cream.webp',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'tinnedIce',
+    category : 'icecream',
+    rating : 4.3,
+    price: 54,
+    img: 'images/icecream/download (5).jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'magicIce',
+    category : 'icecream',
+    rating : 4.3,
+    price: 180,
+    img: 'images/icecream/download (9).jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'coupleIceCream',
+    category : 'icecream',
+    rating : 4.3,
+    price: 79,
+    img: 'images/icecream/images (7).jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'popcornChicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/chicken/chicken.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'copycat-chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 107,
+    img: 'images/chicken/KFC-Chicken-Little-Copycat-Recipe-1024x767.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 120,
+    img: 'images/chicken/Chicken-do-Pyaza.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'fried-chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 190,
+    img: 'images/chicken/download (4).jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Memoni',
+    category : 'chicken',
+    rating : 4.3,
+    price: 25,
+    img: 'images/chicken/9093e9_2e82113c4a3b4d6baab387f33fe309ca_mv2-720x720.webp',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 800,
+    img: 'images/vegetable/download (7).jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'kfc-slaw',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 70,
+    img: 'images/vegetable/KFC-Slaw-6-18-4sq.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg local',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 50,
+    img: 'images/vegetable/download (8).jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'nutrious',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 70,
+    img: 'images/vegetable/FOOD.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Kolhapur',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 100,
+    img: 'images/vegetable/download (10).jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'masala veg',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 150,
+    img: 'images/RiceTypes/egg-biryani.jpeg',
+    quantity: 1
+},
+
+]
+
+export {foodItem};
